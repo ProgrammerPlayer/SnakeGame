0b2be76b24bdc7e8214670ffd7e22e81f6fc827b
ProgrammerPlayer <44946846+ProgrammerPlayer@users.noreply.github.com>
2018-11-11 17:25:35 +0200

    Add files via upload

---
+++ Snake Game/frmMain.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Snake
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMenu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMenu
+            // 
+            this.lblMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMenu.Location = new System.Drawing.Point(0, 0);
+            this.lblMenu.Name = "lblMenu";
+            this.lblMenu.Size = new System.Drawing.Size(300, 201);
+            this.lblMenu.TabIndex = 0;
+            this.lblMenu.Text = "Snake\r\nFoloseste sagetile ca sa  schimbi directia \r\nFoloseste space-ul sa pui pau" +
+    "za\r\nApasa Enter pentru a incepe\r\n\r\n";
+            this.lblMenu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblMenu.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 201);
+            this.Controls.Add(this.lblMenu);
+            this.MaximumSize = new System.Drawing.Size(316, 239);
+            this.MinimumSize = new System.Drawing.Size(316, 239);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake-Score:1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmMain_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMenu;
+    }
+}
+
